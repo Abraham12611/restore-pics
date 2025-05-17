1569e2438750e8ab2b41a20759043ae95838340f
Abraham12611 <abraham.dahunsi@gmail.com>
2025-05-17 13:19:21 +0100

    Update dependencies and configuration for Next.js project

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  transpilePackages: ['next-auth'],
   images: {
     domains: ["upcdn.io", "replicate.delivery", "lh3.googleusercontent.com"],
     unoptimized: true,
+  },
+  experimental: {
+    allowedDevOrigins: ["https://55a9-154-120-84-187.ngrok-free.app"],
   },
   async redirects() {
     return [
